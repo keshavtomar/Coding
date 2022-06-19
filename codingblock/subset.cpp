4552f7f0d80c61a05abec1a99f230f078afe9eb1
Sachin <sachxyz123@gmail.com>
2022-06-19 12:14:50 +0530

    subset

---
+++ codingblock/subset.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool anytwooronesumzero(int *arr, int n) // this will return true if any single element is 0 or sum of any two elements is zero
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            if (arr[i] + arr[j] == 0)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+bool anythreesumzero(int *arr, int n)
+{
+    int count = 0;
+    if (n < 3)
+    {
+        return 0;
+    }
+    for (int i = 0; i < n - 2; i++)
+    {
+        for (int j = i + 1; j < n - 1; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                if (arr[i] + arr[j] + arr[k] == 0)
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+bool allsumzero(int *arr, int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    if (sum == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main(void)
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        bool possible = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        if (anytwooronesumzero(a, n) || anythreesumzero(a, n) || allsumzero(a, n))
+        {
+            possible = 1;
+        }
+        if (possible && n > 0)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+}
--- codingblock/subset_SUM_EASY.cpp
+++ codingblock/subset_SUM_EASY.cpp
@@ -1,35 +1,7 @@
 #include<iostream>
 using namespace std;
-bool subset(long long int a[],long long int n)
+int main()
 {
-    long long int i,j,sum=0,k;
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<n-1;j++)
-        {   sum=0;
-            for(k=i;k<=n-2;k++)
-            {
-                sum=sum+a[k];
-            }
-            if(sum==0) return true;
-        }
-        
-    }
-    return false;
-}
-int main ()
-{
-    long long int m=100000;
-    long long int a[m],n,p,i;
-    cin>>p;
-    for(i=1;i<=p;i++)
-    {
-        cin>>n;
-        for(int j=0;j<n;j++)
-            cin>>a[j];
-        if(subset(a,n))
-            cout<<"Yes";
-        else
-            cout<<"No";
-    }
+    
+    return 0;
 }
